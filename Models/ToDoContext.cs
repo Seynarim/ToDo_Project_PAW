f2ad5e91b3009721dcbb2682536c7f54c28b0c9e
Seynarim_PC <Seynarim_PC@DESKTOP-BOHKL09>
2022-06-24 22:29:27 +0200

    Add project files.

---
+++ Models/ToDoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo_Project_PAW.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,20 @@
+﻿using ToDo_Project_PAW.Models;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace ToDo_Project_PAW.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskId);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+
+
+
+    }
+}
